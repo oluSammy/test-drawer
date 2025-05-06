f428d520d530185b1b06cbcce59bc8894258b26e
olusammy <olumorinsammy@gmail.com>
2025-05-07 00:33:20 +0100

    fix: drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -25,8 +25,8 @@ const DrawerDemo = ({
 }: DrawerProps) => {
   return (
     <Drawer.Root open={isOpen} onOpenChange={onClose}>
+      <Drawer.Overlay className="animate-overlayShow fixed inset-0 z-60 bg-[rgba(9,9,10,0.75)]" />
       <Drawer.Portal>
-        <Drawer.Overlay className="animate-overlayShow fixed inset-0 z-60 bg-[rgba(9,9,10,0.75)]" />
         <Drawer.Content className="border-gray-60 fixed right-0 bottom-0 left-0 z-70 flex h-fit max-h-[97%] flex-col rounded-t-lg border-[0.5px] bg-gray-100 shadow-[0px_10px_38px_-10px_hsla(206,22%,7%,0.35),0px_10px_20px_-15px_hsla(206,22%,7%,0.2)] outline-none">
           {/* <BaseDrawer
             isOpen={isOpen}
