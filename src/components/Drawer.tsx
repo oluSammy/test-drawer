a38ff031e7b2135a79fe5deef042476c2fc61220
olusammy <olumorinsammy@gmail.com>
2025-05-06 19:22:09 +0100

    fix: z index

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -29,13 +29,14 @@ const DrawerDemo = ({
       <Drawer.Portal>
         <Drawer.Overlay className="animate-overlayShow fixed inset-0 z-10 bg-[rgba(9,9,10,0.75)]" />
         <Drawer.Content className="border-gray-60 fixed right-0 bottom-0 left-0 z-60 flex h-fit max-h-[97%] flex-col rounded-t-lg border-[0.5px] bg-gray-100 shadow-[0px_10px_38px_-10px_hsla(206,22%,7%,0.35),0px_10px_20px_-15px_hsla(206,22%,7%,0.2)] outline-none">
-          <BaseDrawer
+          {/* <BaseDrawer
             isOpen={isOpen}
             onClose={onClose}
             title={title}
             content={content}
             nestedDrawer={nestedDrawer}
-          />
+          /> */}
+          <div className="relative mt-10 p-2 z-80">{content}</div>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
