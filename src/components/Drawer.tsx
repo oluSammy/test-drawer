2bd879653f1e80bf7b5e8c697a36bbff8faf5fb7
olusammy <olumorinsammy@gmail.com>
2025-05-07 02:05:43 +0100

    fix: drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -11,8 +11,8 @@ export default function VaulDrawer() {
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] mt-24 max-h-[80dvh] h-fit fixed bottom-0 left-0 right-0 outline-none">
-          <div className="p-4 bg-white rounded-t-[10px] flex-1 overflow-y-auto">
+        <Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] mt-24 max-h-[80vh] fixed bottom-0 left-0 right-0 outline-none overflow-y-auto">
+          <div className="p-4 bg-white rounded-t-[10px]">
             <div className="max-w-md mx-auto space-y-4">
               <div
                 aria-hidden
@@ -28,15 +28,15 @@ export default function VaulDrawer() {
               </p>
               <p className="text-gray-600">
                 But there is this gap. For the first couple years you make
-                stuff, it’s just not that good. It’s trying to be good, it has
-                potential, but it’s not. But your taste, the thing that got you
-                into the game, is still killer. And your taste is why your work
-                disappoints you. A lot of people never get past this phase, they
-                quit.{" "}
+                stuff, it&apos;s just not that good. It&apos;s trying to be
+                good, it has potential, but it&apos;s not. But your taste, the
+                thing that got you into the game, is still killer. And your
+                taste is why your work disappoints you. A lot of people never
+                get past this phase, they quit.{" "}
               </p>
               <p className="text-gray-600">
                 Most people I know who do interesting, creative work went
-                through years of this. We know our work doesn’t have this
+                through years of this. We know our work doesn&apos;t have this
                 special thing that we want it to have. We all go through this.
                 And if you are just starting out or you are still in this phase,
                 you gotta know its normal and the most important thing you can
@@ -47,8 +47,9 @@ export default function VaulDrawer() {
                 one story. It is only by going through a volume of work that you
                 will close that gap, and your work will be as good as your
                 ambitions. And I took longer to figure out how to do this than
-                anyone I’ve ever met. It’s gonna take awhile. It’s normal to
-                take awhile. You’ve just gotta fight your way through.
+                anyone I&apos;ve ever met. It&apos;s gonna take awhile.
+                It&apos;s normal to take awhile. You&apos;ve just gotta fight
+                your way through.
               </p>
             </div>
           </div>
