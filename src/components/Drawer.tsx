9061fc3532f5e68f5af66493fc94213c23abad17
olusammy <olumorinsammy@gmail.com>
2025-05-07 01:03:22 +0100

    fix: drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ReactNode } from "react";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Drawer } from "vaul";
@@ -43,8 +44,8 @@ const DrawerDemo = ({
 
 const BaseDrawer = ({ title, content, nestedDrawer, onClose }: DrawerProps) => {
   return (
-    <>
-      <div className="noScrollbar flex-1 overflow-y-auto rounded-t-[10px]">
+    <div className="z-70">
+      <div className="noScrollbar flex-1 z-70 overflow-y-auto rounded-t-[10px]">
         {title ? (
           <Drawer.Title className={titleClassNames}>
             <button
@@ -68,7 +69,7 @@ const BaseDrawer = ({ title, content, nestedDrawer, onClose }: DrawerProps) => {
         <div className="relative mt-10 p-2">{content}</div>
       </div>
 
-      {nestedDrawer?.map((nested) => (
+      {/* {nestedDrawer?.map((nested) => (
         <Drawer.NestedRoot
           key={nested.title?.toString()}
           open={nested.isOpen}
@@ -88,8 +89,8 @@ const BaseDrawer = ({ title, content, nestedDrawer, onClose }: DrawerProps) => {
             </Drawer.Content>
           </Drawer.Portal>
         </Drawer.NestedRoot>
-      ))}
-    </>
+      ))} */}
+    </div>
   );
 };
 
